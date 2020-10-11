e14b01494e38c7d1b3be554d8d20a1d29d7e3704
Daniel Solá <daniel.sola@genomcore.com>
2020-10-11 19:21:18 +0200

    Tested TaskService

--- src/tasks/services/tasks.service.spec.ts
+++ src/tasks/services/tasks.service.spec.ts
@@ -2,15 +2,22 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
 import { Task } from '../task.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { TaskStatus } from '../tasks.model';
+import { TaskStatus, TaskPriority } from '../tasks.model';
+import { MoreThan, LessThan } from 'typeorm';
+import * as dayjs from 'dayjs';
 
 describe('TasksService', () => {
   let tasksService: TasksService;
+  //Freeze time for testing time-dependant stuff
+  const tk = require('timekeeper');
+  const time = new Date(1330688329321);
+  tk.freeze(time);
 
   const mockTaskRepository = {
     find: jasmine.createSpy(),
     findOne: jasmine.createSpy(),
     save: jasmine.createSpy(),
+    delete: jasmine.createSpy(),
   };
 
   beforeEach(async () => {
@@ -49,6 +56,137 @@ describe('TasksService', () => {
       spyOn(tasksService, 'getOverDueTasks');
       await tasksService.getTasks(TaskStatus.OVERDUE);
       expect(tasksService.getOverDueTasks).toHaveBeenCalled();
+    });
+  });
+
+  describe('getAllTasks method', () => {
+    it('should get all tasks ', async () => {
+      const mockTasks = [
+        { id: 'foo', name: 'Make foo', dueDate: '01/07/1994' },
+      ];
+
+      mockTaskRepository.find.and.returnValue(mockTasks);
+
+      await tasksService.getAllTasks();
+      const findParams = {
+        order: { dueDate: 'DESC', name: 'DESC', priority: 'ASC' },
+      };
+      expect(mockTaskRepository.find).toHaveBeenCalledWith(findParams);
+    });
+  });
+
+  describe('getPendingTasks method', () => {
+    it('should get PENDING tasks ', async () => {
+      await tasksService.getPendingTasks();
+
+      const findPendingTaskParams = {
+        where: { dueDate: MoreThan(new Date()) },
+        order: { dueDate: 'DESC', name: 'DESC', priority: 'ASC' },
+      };
+
+      expect(mockTaskRepository.find).toHaveBeenCalledWith(
+        findPendingTaskParams,
+      );
+    });
+  });
+
+  describe('getOverDueTasks method', () => {
+    it('should get PENDING tasks ', async () => {
+      await tasksService.getOverDueTasks();
+
+      const findOverdueTaskParams = {
+        where: { dueDate: LessThan(new Date()) },
+        order: { dueDate: 'DESC', name: 'DESC', priority: 'ASC' },
+      };
+
+      expect(mockTaskRepository.find).toHaveBeenCalledWith(
+        findOverdueTaskParams,
+      );
+    });
+  });
+
+  describe('createTaskMethod', () => {
+    it('should save task ', async () => {
+      await tasksService.createTask({
+        name: 'Hire Dani',
+        dueDate: '12/10/2020',
+        priority: TaskPriority.HIGH,
+      });
+
+      expect(mockTaskRepository.save).toHaveBeenCalled();
+    });
+  });
+
+  describe('deleteTaskMethod', () => {
+    it('should delete task ', async () => {
+      const id = '2cdcc15f-803e-4287-9707-e61ffde5a274';
+      const mockTask = {
+        name: 'Hire Dani',
+        dueDate: '12/10/2020',
+        priority: TaskPriority.HIGH,
+      };
+      spyOn(tasksService, 'getTaskById').and.returnValue(mockTask);
+      await tasksService.deleteTask(id);
+
+      expect(mockTaskRepository.delete).toHaveBeenCalledWith(mockTask);
+    });
+  });
+
+  describe('updateTaskMethod', () => {
+    it('should update task ', async () => {
+      const id = '2cdcc15f-803e-4287-9707-e61ffde5a274';
+      const mockTask = {
+        name: 'Hire Dani',
+        dueDate: '12/10/2020',
+        priority: TaskPriority.HIGH,
+      };
+
+      spyOn(tasksService, 'getTaskById').and.returnValue(mockTask);
+
+      const newTaskInfo = {
+        priority: TaskPriority.LOW,
+        name: 'Hire Dani ASAP',
+        dueDate: '11/10/2020',
+      };
+
+      await tasksService.updateTask(id, newTaskInfo);
+      const now = dayjs().toISOString();
+
+      expect(mockTaskRepository.save).toHaveBeenCalledWith({
+        ...newTaskInfo,
+        updatedAt: now,
+      });
+    });
+  });
+
+  describe('getTaskById method', () => {
+    it('should retrieve task ', async () => {
+      const id = '2cdcc15f-803e-4287-9707-e61ffde5a274';
+
+      const mockTask = {
+        priority: TaskPriority.LOW,
+        name: 'Hire Dani ASAP',
+        dueDate: '11/10/2020',
+      };
+
+      mockTaskRepository.findOne.and.returnValue(mockTask);
+
+      await tasksService.getTaskById(id);
+
+      expect(mockTaskRepository.findOne).toHaveBeenCalledWith({ id });
+    });
+
+    it('should throw error if task is not found ', async () => {
+      const id = '2cdcc15f-803e-4287-9707-e61ffde5a274';
+
+      mockTaskRepository.findOne.and.returnValue(null);
+
+      try {
+        await tasksService.getTaskById(id);
+      } catch (error) {
+        expect(mockTaskRepository.findOne).toHaveBeenCalledWith({ id });
+        expect(error).toBeDefined();
+      }
     });
   });
 });
