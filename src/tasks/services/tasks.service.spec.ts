165a2386cdbed323afcbe341176288749ce733cf
Daniel Solá <daniel.sola@genomcore.com>
2020-10-11 18:50:56 +0200

    Added test for getTask method

--- src/tasks/services/tasks.service.spec.ts
+++ src/tasks/services/tasks.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
 import { Task } from '../task.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { TaskStatus } from '../tasks.model';
 
 describe('TasksService', () => {
   let tasksService: TasksService;
@@ -29,5 +30,25 @@ describe('TasksService', () => {
 
   it('should be defined', () => {
     expect(tasksService).toBeDefined();
+  });
+
+  describe('getTasks method', () => {
+    it('should get all tasks if no status is provided', async () => {
+      spyOn(tasksService, 'getAllTasks');
+      await tasksService.getTasks(null);
+      expect(tasksService.getAllTasks).toHaveBeenCalled();
+    });
+
+    it('should get PENDING tasks', async () => {
+      spyOn(tasksService, 'getPendingTasks');
+      await tasksService.getTasks(TaskStatus.PENDING);
+      expect(tasksService.getPendingTasks).toHaveBeenCalled();
+    });
+
+    it('should get OVERDUE tasks', async () => {
+      spyOn(tasksService, 'getOverDueTasks');
+      await tasksService.getTasks(TaskStatus.OVERDUE);
+      expect(tasksService.getOverDueTasks).toHaveBeenCalled();
+    });
   });
 });
