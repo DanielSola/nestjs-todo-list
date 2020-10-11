223a429d18a19c9c5d54a57b87ecda76c16e0d47
Daniel Solá <daniel.sola@genomcore.com>
2020-10-11 16:55:08 +0200

    Added methods getOverDueTasks and getPendingTasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import * as uuid from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, MoreThan, LessThan } from 'typeorm';
 import { Task } from './task.entity';
 
 @Injectable()
@@ -16,15 +16,27 @@ export class TasksService {
     return tasks.map(this.setTaskIsOverDue);
   }
 
+  async getPendingTasks(): Promise<Task[]> {
+    return await this.taskRepository.find({
+      where: { dueDate: MoreThan(new Date()) },
+    });
+  }
+
+  async getOverDueTasks(): Promise<Task[]> {
+    return await this.taskRepository.find({
+      where: { dueDate: LessThan(new Date()) },
+    });
+  }
+
   async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
     const task: Task = { ...createTaskDto, id: uuid.v4() };
 
     return await this.taskRepository.save(task);
   }
 
-  private setTaskIsOverDue = (task: Task) => {
+  private setTaskIsOverDue(task: Task) {
     const isOverDue = new Date(task.dueDate) < new Date();
 
     return { ...task, isOverDue };
-  };
+  }
 }
